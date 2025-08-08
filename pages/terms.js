c8fd5436a13f7a143c940c2cb2de1f0b5b6b2736
chetan7528 <kaklotarchetan7528@gmail.com>
2025-08-08 18:30:43 +0530

    minor chnges

--- pages/terms.js
+++ pages/terms.js
@@ -119,6 +119,15 @@ function Terms() {
                 </div>
             </div>
 
+            <div className="fixed md:bottom-8 bottom-4 right-8 z-40" onClick={() => window.open(`https://api.whatsapp.com/send/?phone=+918638597393&text=Hi,&app_absent=0`, '_black')}>
+                <button className="bg-white text-[#0D34BF] px-6 py-3 rounded-full shadow-lg hover:shadow-xl transition-all flex items-center gap-2 font-medium">
+                    <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                    </svg>
+                    Write to us
+                </button>
+            </div>
+
             <Footer />
         </>
     )
